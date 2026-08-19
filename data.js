08909f5733c8d4b1643da0c65e1678e6a3d4a7cd
HgChvz-dev <hugoalfredochavezlopez@gmail.com>
2026-08-19 14:11:55 -0400

    feat: Recursos

---
+++ data.js
@@ -0,0 +1,26 @@
+export const menuArray = [
+    {
+        id: 0,
+        name: "Pizza",
+        ingredients: ["pepperoni", "mushrom", "mozarella"],
+        price: 14,
+        img: "images/pizza.png",
+        alt: "A piece of pizza with pepperoni",
+    },
+    {
+        id: 1,
+        name: "Hamburger",
+        ingredients: ["beef", "cheese", "lettuce"],
+        price: 12,
+        img: "images/hamburger.png",
+        alt: "A hamburger with cheese and lettuce",
+    },
+    {
+        id: 2,
+        name: "Beer",
+        ingredients: ["grain, hops, yeast, water"],
+        price: 12,
+        img: "images/beer.png",
+        alt: "A glass of beer with foam on top",
+    }
+]
